9d273b9f81a53fc11cd1a49033a190611947219d
arllainm <arllain@gmail.com>
2020-02-13 08:28:14 -0300

    first commit

---
+++ src/components/TechItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TechItem({ tech }) {
+  return (
+    <li>
+      {tech}
+      <button onClick={() => this.handleDelete(tech)} type='button'>
+        Remover
+      </button>
+    </li>
+  );
+}
+export default TechItem;
